5c3784e13c59448d1d5d2c0b8ffd0ea9aefff93d
Wesaal <WesaalEbraheem@hotmail.com>
2026-05-21 22:13:26 +0300

    Update main.c
    fix: fix prompt from #cisfun$ to ($) and add command counter for error messages

--- main.c
+++ main.c
@@ -12,12 +12,12 @@ int main(int argc __attribute__((unused)), char **argv)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
+	int count = 1;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
-
+			write(STDOUT_FILENO, "($) ", 4);
 		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 		{
@@ -25,16 +25,13 @@ int main(int argc __attribute__((unused)), char **argv)
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
 		if (line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
-
 		if (line[0] == '\0')
 			continue;
-
-		execute(line, argv[0]);
+		execute(line, argv[0], count);
+		count++;
 	}
-
 	free(line);
 	return (0);
 }
