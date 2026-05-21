f666a8f16c7daf2b559dc60e9a9d9fc42bb19bdc
Wesaal <WesaalEbraheem@hotmail.com>
2026-05-21 22:12:02 +0300

    Update shell.h
    fix: update shell.h with new function prototypes

--- shell.h
+++ shell.h
@@ -1,17 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
 extern char **environ;
-
 char *trim(char *str);
 char **tokenize(char *str);
-void execute(char *cmd, char *prog);
-
+void execute(char *cmd, char *prog, int count);
+char *find_in_path(char *cmd);
+int handle_builtin(char **args);
 #endif /* SHELL_H */
